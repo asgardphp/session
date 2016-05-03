c635fe4acff242bce9cbcd895d62dcca378edb7b
michel hognerud <leyou.m@gmail.com>
2016-05-03 14:07:17 +0200

    fix

--- Bundle.php
+++ Bundle.php
@@ -3,10 +3,8 @@ namespace Asgard\Session;
 
 class Bundle extends \Asgard\Core\BundleLoader {
 	public function buildContainer(\Asgard\Container\ContainerInterface $container) {
-		$container->register('data', function($container) { return new Data($container->get('db')); } );
 	}
 
 	public function run(\Asgard\Container\ContainerInterface $container) {
-		
 	}
 }
